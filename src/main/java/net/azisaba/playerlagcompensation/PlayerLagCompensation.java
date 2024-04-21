5365a2874782dd284b40cf0f8754d567f72082e0
matsu1213 <matsu1213mgal@gmail.com>
2024-04-21 10:57:51 +0900

    Initial commit

---
+++ src/main/java/net/azisaba/playerlagcompensation/PlayerLagCompensation.java
@@ -0,0 +1,17 @@
+package net.azisaba.playerlagcompensation;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlayerLagCompensation extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
